0bc74fd23bee9ece5e2478e18492020a6f421498
calypso <84587581+calypsohebrard@users.noreply.github.com>
2025-05-15 15:36:11 +0200

    added chrono

--- script.js
+++ script.js
@@ -2,10 +2,79 @@ const back = document.getElementsByClassName('back-card');
 const front = document.getElementsByClassName('front-card');
 const card = document.querySelectorAll('.card');
 let chrono = document.getElementsByClassName('chronometre');
-const resBtn = document.getElementsById('reset-game-btn');
+const resBtn = document.getElementById('reset-game-btn');
+
+
+const chronoText = document.getElementById("chronoText");
+const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
+const stopBtn = document.getElementById("stop");
+
+
 
 let card1;
 let card2; 
+let cardFlipped = false;
+
+let minutes = 0;
+let seconds = 0;
+let timeout;
+let isStopped = true;
+
+const start = () => {
+  if(isStopped) {
+    isStopped = false;
+    passTime();
+  }
+};
+
+const stop = () => {
+  if(!isStopped) {
+    isStopped = true;
+    clearTimeout(timeout);
+  }
+}
+
+const passTime = () => {
+  if(isStopped) {
+    return;
+  }
+  seconds = parseInt(seconds);
+  minutes = parseInt(minutes);
+  
+  seconds++;
+  if(seconds === 60) {
+    minutes++;
+    seconds = 0;
+  }
+
+  if(minutes === 60) {
+    minutes = 0;
+  }
+
+  //affichage 
+  if(seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  if(minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  chronoText.textContent = `${minutes}:${seconds}`;
+  timeout = setTimeout(passTime, 1000);
+}
+
+const reset = () => {
+  chronoText.textContent = `00:00`;
+  isStopped = true;
+  seconds = 0;
+  minutes = 0;
+  clearTimeout(timeout);
+}
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
+
 
 function shuffle() {
   card.forEach(cards => {
@@ -15,28 +84,20 @@ function shuffle() {
 }
 
 function flip(card){
+    cardFlipped = true;
     card.classList.add('flip');
 }
 
+card.addEventListener("click", flip)
+resBtn.addEventListener("click", shuffle);
 
 function isSame(){
-    if (card1.data.image === card2.data.image){
+    if (card1.className = card2.className){
         return;
     }else{
         card1.classList.remove('flip');
         card2.classList.remove('flip');
         return;
     }
-}
 
-function win(){
-   alert("Well done you win the game");
 }
- 
-function reset(){
-    (card.forEach(cards => card.classList.remove('flip')))
-    shuffle();
-}
-
-card.forEach(card => card.addEventListener('click', flip));
-resBtn.addEventListener("click", reset);
