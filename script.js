67e3f3a951aecb933536e91a70b2a4633f372ba4
Olivier <olivier.forabosco05@gmail.com>
2025-05-23 14:07:07 +0200

    Amélioration du shuffle + function win mise à jour

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ function win(){
   // Mise à jour des minutes/secondes si nécessaire
   const currentSec = seconds < 10 ? `0${seconds}` : seconds;
   const currentMin = minutes < 10 ? `0${minutes}` : minutes;
-
+if(diff.value === "easy"){
   if (seconds <= 15 && minutes === 0) {
     winTxt.innerText = `Well done! You win in ${currentMin} minutes : ${currentSec} seconds — You are the GOAT!`;
   } else if (seconds <= 30 && minutes === 0) {
@@ -153,7 +153,19 @@ function win(){
   } else {
     winTxt.innerText = `You finished in ${currentMin} minutes : ${currentSec} seconds — You really need to train!`;
   }
-
+}else{
+  if (seconds <= 30 && minutes === 0) {
+    winTxt.innerText = `Well done! You win in ${currentMin} minutes : ${currentSec} seconds — You are the GOAT!`;
+  } else if (seconds <= 45 && minutes === 0) {
+    winTxt.innerText = `Well done! You win in ${currentMin} minutes : ${currentSec} seconds — That's very good!`;
+  } else if (seconds >= 0 && minutes === 1) {
+    winTxt.innerText = `Well done! You win in ${currentMin} minutes : ${currentSec} seconds — That's good enough!`;
+  } else if (seconds >= 30 && minutes === 1){
+    winTxt.innerText = `You finished in ${currentMin} minutes : ${currentSec} seconds — You really need to train!`;
+  } else if (minutes >= 2){
+    winTxt.innerText = `You finished in ${currentMin} minutes : ${currentSec} seconds — Bro... your sleeping or what ?`
+  }
+}
   movementsTxt.innerText = `Moves: ${movements / 2}`;
 }
 
@@ -164,8 +176,15 @@ function resetText(){
 } 
 
 //Function du changement de difficulté
-
-const hard = document.querySelector("#hard");
+const hard = document.querySelectorAll(".hard")
+const hard1 = document.querySelector("#hard1");
+const hard2 = document.querySelector("#hard2");
+const hard3 = document.querySelector("#hard3");
+const hard4 = document.querySelector("#hard4");
+const hard5 = document.querySelector("#hard5");
+const hard6 = document.querySelector("#hard6");
+const hard7 = document.querySelector("#hard7");
+const hard8 = document.querySelector("#hard8");
 const diff = document.querySelector(".difficulty");
 
 diff.addEventListener("change",(e) => {
@@ -175,70 +194,59 @@ diff.addEventListener("change",(e) => {
 function diffChange(){
   if(diff.value === "hard"){
     totalPairs = 8;
+    hard.forEach(card => card.classList.add("visible"));
     resetChrono();
     cardElements.forEach(card => card.classList.remove('flip'));
     resetText();
     movementsTxt.innerText = "";
     movements = 0;
-    hard.innerHTML = `<div class="row">
-      <div class="card" data-image="panther">
+    hard1.innerHTML = `
+        <div class="card-inner">
+          <div class="back-card"><img src="images/question-mark.jpg"></div>
+          <div class="front-card"><img src="images/panther.jpg"></div>
+        </div>`
+    hard2.innerHTML=`
+        <div class="card-inner">
+          <div class="back-card"><img src="images/question-mark.jpg"></div>
+          <div class="front-card"><img src="images/eagle.jpg"></div>
+        </div>`
+      hard3.innerHTML=`
+        <div class="card-inner">
+          <div class="back-card"><img src="images/question-mark.jpg"></div>
+          <div class="front-card"><img src="images/elephant.jpg"></div>
+        </div>`
+      hard4.innerHTML=`
         <div class="card-inner">
           <div class="back-card"><img src="images/question-mark.jpg"></div>
           <div class="front-card"><img src="images/panther.jpg"></div>
         </div>
-      </div>
-      <div class="card" data-image="eagle">
+    `
+      hard5.innerHTML=`
+        <div class="card-inner">
+          <div class="back-card"><img src="images/question-mark.jpg"></div>
+          <div class="front-card"><img src="images/elephant.jpg"></div>
+        </div>`
+      hard6.innerHTML=`
+        <div class="card-inner">
+          <div class="back-card"><img src="images/question-mark.jpg"></div>
+          <div class="front-card"><img src="images/giraffe.jpg"></div>
+        </div>`
+      hard7.innerHTML=`
+        <div class="card-inner">
+          <div class="back-card"><img src="images/question-mark.jpg"></div>
+          <div class="front-card"><img src="images/giraffe.jpg"></div>
+        </div>`
+      hard8.innerHTML=`
         <div class="card-inner">
           <div class="back-card"><img src="images/question-mark.jpg"></div>
           <div class="front-card"><img src="images/eagle.jpg"></div>
         </div>
-      </div>
-      <div class="card" data-image="elephant">
-        <div class="card-inner">
-          <div class="back-card"><img src="images/question-mark.jpg"></div>
-          <div class="front-card"><img src="images/elephant.jpg"></div>
-        </div>
-      </div>
-      <div class="card" data-image="panther">
-        <div class="card-inner">
-          <div class="back-card"><img src="images/question-mark.jpg"></div>
-          <div class="front-card"><img src="images/panther.jpg"></div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="card" data-image="elephant">
-        <div class="card-inner">
-          <div class="back-card"><img src="images/question-mark.jpg"></div>
-          <div class="front-card"><img src="images/elephant.jpg"></div>
-        </div>
-      </div>
-      <div class="card" data-image="giraffe">
-        <div class="card-inner">
-          <div class="back-card"><img src="images/question-mark.jpg"></div>
-          <div class="front-card"><img src="images/giraffe.jpg"></div>
-        </div>
-      </div>
-      <div class="card" data-image="giraffe">
-        <div class="card-inner">
-          <div class="back-card"><img src="images/question-mark.jpg"></div>
-          <div class="front-card"><img src="images/giraffe.jpg"></div>
-        </div>
-      </div>
-      <div class="card" data-image="eagle">
-        <div class="card-inner">
-          <div class="back-card"><img src="images/question-mark.jpg"></div>
-          <div class="front-card"><img src="images/eagle.jpg"></div>
-        </div>
-      </div>
-    </div>
-`
+      `
+    console.log(movements);
     cardElements = document.querySelectorAll('.card');
     document.querySelectorAll('.card').forEach(card => {
       card.addEventListener('click', () => {
         flip(card);
-        movements++;
       });
       card.addEventListener('click', () => {
         start();
@@ -246,6 +254,7 @@ function diffChange(){
     });
     shuffle();
   }else{
+    hard.forEach(card => card.classList.remove("visible"));
     totalPairs = 4;
     hard.innerHTML = ``;
     resetChrono();
